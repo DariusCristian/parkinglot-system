5d42ca67b7ce0ab275b5182054395ae31ef13480
Varvara Robert <varvararobert41@gmail.com>
2026-01-06 17:18:03 +0200

    5th

---
+++ Areas/Identity/Data/ParkingLotSystemIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ParkingLotSystem.Areas.Identity.Data
+{
+    public class ParkingLotSystemIdentityContext : IdentityDbContext<IdentityUser>
+    {
+        public ParkingLotSystemIdentityContext(DbContextOptions<ParkingLotSystemIdentityContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ParkingLotSystem.Data;
+using ParkingLotSystem.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+
+// App DB (your existing one)
 builder.Services.AddDbContext<ParkingLotSystemContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ParkingLotSystemContext") ?? throw new InvalidOperationException("Connection string 'ParkingLotSystemContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ParkingLotSystemContext")));
+
+// Identity DB (new one)
+builder.Services.AddDbContext<ParkingLotSystemIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ParkingLotSystemIdentityContext")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ParkingLotSystemIdentityContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
